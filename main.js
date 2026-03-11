e4fac8a523bca5e8e42f788ae5b3363c0db3ef76
Krnsand <krnsandin@gmail.com>
2026-03-11 17:19:38 +0100

    Add new video

--- main.js
+++ main.js
@@ -26,7 +26,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
   var allowedVideos = {
     'videos/studio.mp4': true,
-    'videos/paddock.mp4': true
+    'videos/paddock.mp4': true,
+    'videos/mounted_games_1.mp4': true
   };
 
   var params = new URLSearchParams(window.location.search);
